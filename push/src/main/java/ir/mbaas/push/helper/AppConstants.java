2f7fa7e66f8a1ff2de7eed69299b1f44c23a846c
Mahdi Moradi <moradi.mehdi@gmail.com>
2016-04-06 21:00:01 +0430

    Initial commit.

---
+++ push/src/main/java/ir/mbaas/push/helper/AppConstants.java
@@ -0,0 +1,13 @@
+package ir.mbaas.push.helper;
+
+public interface AppConstants {
+    // Note: emulator IP for host's localhost is 10.0.2.2
+    String INSTANCE_URL = "http://push.mbaas.ir/api/v2";
+    String MBAAS_SERVICE  = "Push";
+    String AUTH_SVC = "user";
+    String DB_SVC = "db/_table";
+    // API key for your app goes here, see apps tab in admin console
+    String API_KEY = "38898fbba5086b2a653cf34400ee76aaeaeb9a87f0caa39eb106bfcc8963a895";
+
+    int VERSION_NUMBER = 1;
+}
