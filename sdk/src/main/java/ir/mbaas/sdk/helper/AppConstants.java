4f43779af58fc23b418161d4fdde55946de397ff
Mahdi Moradi <moradi.mehdi@gmail.com>
2016-06-04 13:16:19 +0430

    URL is changed.

--- sdk/src/main/java/ir/mbaas/sdk/helper/AppConstants.java
+++ sdk/src/main/java/ir/mbaas/sdk/helper/AppConstants.java
@@ -2,20 +2,15 @@ package ir.mbaas.sdk.helper;
 
 public interface AppConstants {
     // Note: emulator IP for host's localhost is 10.0.2.2
-    String INSTANCE_URL = "http://mb-lab.mbaas.ir/api/v2";
-    String GCM_SERVICE  = "MBAAS_PUSH";
+    String INSTANCE_URL = "http://push.mbaas.ir/api/v2";
+    String GCM_SERVICE  = "mbaas_push";
 
     String GCM_REGISTER_API  = "register";
     String GCM_UPDATE_API  = "update";
     String GCM_DELIVER_API   = "delivery";
     String GCM_LOCATIONS_API = "locations";
 
-    String AUTH_SVC = "user";
-    String DB_SVC = "db/_table";
-    // API key for your app goes here, see apps tab in admin console
-    String API_KEY = "b48a0426f5943e7196c4523dc661a60144f0fe2c70deb3fb528d90ed33aa0f02";
-
-    int VERSION_NUMBER = 1;
+    String API_KEY = "dc7dec8087f417c0c4212309415804690abe38127dfb9bfa5c442633170b068f";
 
     String MBAAS_BASE_URL = "http://mbaas.ir/";
 
